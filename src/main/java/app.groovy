9d8c8418cbd218b656b5edcb0eb71b415ea62821
Jeffery Finley <jeff@jefferyfinley.com>
2016-10-11 15:49:08 -0500

    seeing if the TravisCI works now

--- src/main/java/app.groovy
+++ src/main/java/app.groovy
@@ -2,6 +2,6 @@
 class ThisWillActuallyRun {
 	@RequestMapping("/")
 	String home() {
-		return "Hello Groovy Pigs Live in this World!\nAnd another line to see if this thing will actually build\nAnd another change to see if the CI being checked was the thing that kept it from updating"
+		return "Hello Groovy Pigs Live in this World!\nAnd another line to see if this thing will actually build\nAnd another change to see if the CI being checked was the thing that kept it from updating.  And another line to see if the CI is working"
 }
 }
