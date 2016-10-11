aac1ff6fcc4f446841a3773fdaf68d3c0ecfd9f6
Jeffery Finley <jeff@jefferyfinley.com>
2016-10-11 15:13:56 -0500

    one more time if we try because I unchecked the CI in the auto dploymnt box

--- src/main/java/app.groovy
+++ src/main/java/app.groovy
@@ -2,6 +2,6 @@
 class ThisWillActuallyRun {
 	@RequestMapping("/")
 	String home() {
-		return "Hello Groovy Pigs Live in this World!\nAnd another line to see if this thing will actually build"
+		return "Hello Groovy Pigs Live in this World!\nAnd another line to see if this thing will actually build\nAnd another change to see if the CI being checked was the thing that kept it from updating"
 }
 }
