71015fa4863f4ab17645bf1f990f3ae4790793ba
Jeffery Finley <jeff@jefferyfinley.com>
2016-10-11 15:05:30 -0500

    see if this thing will actually work.  It did not last time but I do not know if I was too impatient

--- src/main/java/app.groovy
+++ src/main/java/app.groovy
@@ -2,6 +2,6 @@
 class ThisWillActuallyRun {
 	@RequestMapping("/")
 	String home() {
-		return "Hello Groovy Pigs Live in this World!\n"
+		return "Hello Groovy Pigs Live in this World!\nAnd another line to see if this thing will actually build"
 }
 }
